87385fb1e5db24ec080d6c845cddebb95d17d9bd
ronald cortez <ronaldcortez@ronalds-Mac-mini.local>
2024-11-13 15:44:04 -0500

    fixed query

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -25,13 +25,28 @@
               v-on:keyup="processQuery"
               class="mr-sm-2 form-control"
               id="nav-search"
-              :value="inputVal"
               autocomplete="off">
     </div>
-    <div class="row main-content">
+    <div class="col-md-12 main-content">
       <div class="col-md-12">
         <div class="p-4">
-          
+          <div class="row query-section">
+            <div class="query-message">
+              <p>
+                <i class="fa fa-thumbs-down" v-if="queryResult=='Error'"></i>
+                {{queryResult}}
+              </p>
+              <div class="spinner-border" role="status" v-if="queryResult=='Loading' ">
+                <span class="sr-only">Loading...</span>
+              </div>
+            </div>
+            <div class="col-md-4" :id=" fullWidth?'fw-'+item.id :''"
+                  v-for="item in content"
+                  :key="item.id">
+                  
+
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -44,7 +59,9 @@
 //import home from './pages/home.vue'   <home /> import {routes} from './routes'
 import HeaderDiv from './components/HeaderDiv.vue'
 import FooterrDiv from './components/FooterDiv.vue'
-
+import axios from 'axios'
+import {URL} from './api-config'
+import {apiData} from './apiData'
 
 export default {
   name: 'App', 
@@ -58,7 +75,11 @@ export default {
       hideForm: true,
       hideRequired:true,
       inputVal: '',
-      showNavBar: false
+      showNavBar: false,
+      btnLabel:'Copy',
+      queryResult:'',
+      fullWidth:true,
+      content: []
 
 
     }
@@ -66,7 +87,13 @@ export default {
   methods: {
 
     processQuery(e){
+
+      this.queryResult ='Loading'
       let val = e.target.value
+
+      console.log('query',val)
+
+      /*
       if(val.length > 0 ){
         this.$root.$emit('eventing', val)
       }
@@ -78,15 +105,55 @@ export default {
         this.hideRequired = false;
         setInterval( () => this.hideRequired = true    ,2000)
       }
+      */
+
+      this.queryResult = 'Loading'
+
+      let queryUrl = `${URL.apiUrl}/cheatsheet-builder.php?q=`+ encodeURI((val).trim())
+      axios({method:'get',url:queryUrl})
+
+      .then(data => {
+        if( data.data.length == 0) this.queryResult = 'No Results'
+
+        else if (typeof data.data == 'string'){
+          this.db_error(val)
+        }
+        else{
+          this.content = data.data
+        }
+
+
+      })
+      .catch(error => {
+
+        console.log(error,'stupid')
+        this.db_error(val)
+
+
+      })//catch end
+
+
     },
     toggleNavbar(){
       this.showNavBar = !this.showNavBar;
 
+    },
+    db_error(data) {
+      this.queryResult = ''
+      this.content = apiData.filter( d => {
+                        if ( ((`${d.lng} ${d.code} ${d.dsc} ${d.title}`)).toLowerCase().indexOf(data.toLowerCase()) > -1 )
+                          return d
+                        else
+                            this.queryResult = 'No Results'
+                      })
+
+      this.content = new Array(...this.content)
+      console.log('content',this.content, this.content.length)
     }
   },
   beforeUpdate(){
     //clears input field
-    this.inputVal = ''
+    //this.inputVal = ''
     document.querySelector('#navbarCollapse').classList.remove("show")
   }
 }
