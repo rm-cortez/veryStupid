b178e5a16e1f738bf91f8c9e06342fe3d67c6aae
ronald cortez <ronaldcortez@ronalds-Mac-mini.local>
2024-11-15 12:41:28 -0500

    added el no seas imbecil file

---
+++ angular-project/src/app/data-service.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataServiceService {
+
+ apiData = null
+ apiUrl= null
+ icons =
+ [
+   { rn:0, id:1	, icon: 'fa-arrows-alt'},
+   { rn:0, id:2	, icon: 'fa-expand'},
+   { rn:0, id:3	, icon: 'fa-pause'},
+   { rn:0, id:4	, icon: 'fa-user-circle'},
+   { rn:0, id:5	, icon: 'fa-hourglass-end'},
+   { rn:0, id:6	, icon: 'fa-star'},
+   { rn:0, id:7	, icon: 'fa-arrow-down'},
+   { rn:0, id:8	, icon: 'fa-step-backward'},
+   { rn:0, id:9	, icon: 'fa-forward'},
+   { rn:0, id:10	, icon: 'fa-arrow-up'},
+   { rn:0, id:11	, icon: 'fa-stop'},
+   { rn:0, id:12	, icon: 'fa-eject'},
+   { rn:0, id:13	, icon: 'fa-arrows-alt'},
+   { rn:0, id:14	, icon: 'fa-expand'},
+   { rn:0, id:15	, icon: 'fa-pause'},
+   { rn:0, id:16	, icon: 'fa-user-circle'},
+   { rn:0, id:17	, icon: 'fa-hourglass-end'},
+   { rn:0, id:18	, icon: 'fa-star'},
+   { rn:0, id:19	, icon: 'fa-arrow-down'},
+   { rn:0, id:20	, icon: 'fa-step-backward'},
+   { rn:0, id:21	, icon: 'fa-forward'},
+   { rn:0, id:22	, icon: 'fa-arrow-up'},
+   { rn:0, id:23	, icon: 'fa-stop'},
+   { rn:0, id:24	, icon: 'fa-eject'}
+ ]
+
+  constructor(private http:HttpClient) { 
+
+  }
+
+ getData(params = ''){
+
+    return this.http.get(`${this.apiUrl}/angular-builder.php?${params}`)
+  }
+
+
+
+}
